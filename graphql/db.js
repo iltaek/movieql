c8d311ec99bfa9668b222bd551ca8af65900b61f
iltaek <iltaekkwon@gmail.com>
2021-08-22 01:19:16 +0900

    Wrapping a REST API with GraphQL

--- graphql/db.js
+++ graphql/db.js
@@ -1,53 +1,17 @@
-let movies = [
-  {
-    id: 0,
-    name: 'Memento',
-    score: 19,
-  },
-  {
-    id: 1,
-    name: 'Avengers - The new one',
-    score: 42,
-  },
-  {
-    id: 2,
-    name: 'The Godfather I',
-    score: 99,
-  },
-  {
-    id: 3,
-    name: 'Logan',
-    score: 22,
-  },
-  {
-    id: 4,
-    name: 'X-man',
-    score: 10,
-  },
-];
+import axios from 'axios';
 
-export const getById = id => {
-  const filteredMovie = movies.filter(movie => movie.id === id);
-  return filteredMovie[0];
-};
+const LIST_MOVIES_URL = 'https://yts.mx/api/v2/list_movies.json?';
 
-export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  }
-  return false;
-};
-
-export const getMovies = () => movies;
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: getMovies().length,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating,
+    },
+  });
+  return movies;
 };
